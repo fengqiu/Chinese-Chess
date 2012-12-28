20efe4f7c1ead3bd97c90c612f06ca5d37100c45
Adam Dong <fengqiu.dong@gmail.com>
2012-12-28 17:13:41 +0800

    asdasd

--- GIT TEST/ViewController.m
+++ GIT TEST/ViewController.m
@@ -18,10 +18,7 @@
 {
     [super viewDidLoad];
     NSLog(@"superman is here don't worry kids he'll fuck y'll!");
-    int i;
-    i=5;
-    NSLog(@"%i", i);
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
